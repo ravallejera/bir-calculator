324745c6ec9744cedda6e1099269db6d6e62e4dd
Alvin Vallejera <ravallejera@yahoo.com>
2014-08-22 18:49:26 +0800

    Initial commit

---
+++ data.js
@@ -0,0 +1,65 @@
+[
+	{
+		"period":
+			[
+				{
+					"monthly":
+						{
+							"status":
+								[
+									{
+										"sme":
+											[
+												{ "id": 1, "comansation_level": 1	   	},
+												{ "id": 2, "comansation_level": 4167	},
+												{ "id": 3, "comansation_level": 5000	},
+												{ "id": 4, "comansation_level": 6667	},
+												{ "id": 5, "comansation_level": 10000	},
+												{ "id": 6, "comansation_level": 15833	},
+												{ "id": 7, "comansation_level": 25000	},
+												{ "id": 8, "comansation_level": 45833	},
+												
+											]
+											
+									},
+									{
+										"me1s1":
+											[
+												{ "id": 1, "comansation_level": 1		},
+												{ "id": 2, "comansation_level": 6250	},
+												{ "id": 3, "comansation_level": 7083	},
+												{ "id": 4, "comansation_level": 8750	},
+												{ "id": 5, "comansation_level": 12083	},
+												{ "id": 6, "comansation_level": 17917	},
+												{ "id": 7, "comansation_level": 27083	},
+												{ "id": 8, "comansation_level": 47917	},
+												
+											]
+											
+									},
+								]
+								
+						}
+				}
+			]
+	},
+	{
+		"excemption": 
+			[
+				{
+					"monthly":
+						[
+							{ "id": 1, "value": 0,  		"percent": 0 },
+							{ "id": 2, "value": 0,  		"percent": .05},
+							{ "id": 3, "value": 41.67,  	"percent": .1},
+							{ "id": 4, "value": 208.33,  	"percent": .15},
+							{ "id": 5, "value": 708.33,  	"percent": .20},
+							{ "id": 6, "value": 1875,  		"percent": .25},
+							{ "id": 7, "value": 4166.67, 	"percent": .30},
+							{ "id": 8, "value": 10416.67, 	"percent": .32},
+							
+						]
+				}
+			]
+	}
+]
