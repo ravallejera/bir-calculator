e333bc98b30504ababafb8e049427b9c24a90484
Alvin Vallejera <ravallejera@yahoo.com>
2014-08-22 19:12:10 +0800

    update data based

--- data.js
+++ data.js
@@ -1,4 +1,17 @@
 [
+    {
+        "percent":
+            [
+                { "id": 1, "value": 0     },
+                { "id": 2, "value": .05 },
+                { "id": 3, "value": .1 },
+                { "id": 4, "value": .15 },
+                { "id": 5, "value": .20 },
+                { "id": 6, "value": .25 },
+                { "id": 7, "value": .30 },
+                { "id": 8, "value": .32 }
+            ]
+    },
 	{
 		"period":
 			[
@@ -8,35 +21,155 @@
 							"status":
 								[
 									{
-										"sme":
+										"s":
 											[
-												{ "id": 1, "comansation_level": 1	   	},
-												{ "id": 2, "comansation_level": 4167	},
-												{ "id": 3, "comansation_level": 5000	},
-												{ "id": 4, "comansation_level": 6667	},
-												{ "id": 5, "comansation_level": 10000	},
-												{ "id": 6, "comansation_level": 15833	},
-												{ "id": 7, "comansation_level": 25000	},
-												{ "id": 8, "comansation_level": 45833	},
+												{ "id": 1, "compansation_level": 1	   	},
+												{ "id": 2, "compansation_level": 4167	},
+												{ "id": 3, "compansation_level": 5000	},
+												{ "id": 4, "compansation_level": 6667	},
+												{ "id": 5, "compansation_level": 10000	},
+												{ "id": 6, "compansation_level": 15833	},
+												{ "id": 7, "compansation_level": 25000	},
+												{ "id": 8, "compansation_level": 45833	}
 												
 											]
 											
 									},
 									{
-										"me1s1":
+										"s1":
 											[
-												{ "id": 1, "comansation_level": 1		},
-												{ "id": 2, "comansation_level": 6250	},
-												{ "id": 3, "comansation_level": 7083	},
-												{ "id": 4, "comansation_level": 8750	},
-												{ "id": 5, "comansation_level": 12083	},
-												{ "id": 6, "comansation_level": 17917	},
-												{ "id": 7, "comansation_level": 27083	},
-												{ "id": 8, "comansation_level": 47917	},
+												{ "id": 1, "compansation_level": 1		},
+												{ "id": 2, "compansation_level": 6250	},
+												{ "id": 3, "compansation_level": 7083	},
+												{ "id": 4, "compansation_level": 8750	},
+												{ "id": 5, "compansation_level": 12083	},
+												{ "id": 6, "compansation_level": 17917	},
+												{ "id": 7, "compansation_level": 27083	},
+												{ "id": 8, "compansation_level": 47917	}
 												
 											]
 											
 									},
+                                    {
+                                        "s2":
+											[
+												{ "id": 1, "compansation_level": 1 },
+												{ "id": 2, "compansation_level": 8333 },
+												{ "id": 3, "compansation_level": 9167 },
+												{ "id": 4, "compansation_level": 10833 },
+												{ "id": 5, "compansation_level": 14167 },
+												{ "id": 6, "compansation_level": 20000 },
+												{ "id": 7, "compansation_level": 29167 },
+												{ "id": 8, "compansation_level": 47917 }
+
+											]
+
+                                    },
+                                    {
+                                        "s3":
+											[
+												{ "id": 1, "compansation_level": 1 },
+												{ "id": 2, "compansation_level": 10417 },
+												{ "id": 3, "compansation_level": 11250 },
+												{ "id": 4, "compansation_level": 12917 },
+												{ "id": 5, "compansation_level": 16250 },
+												{ "id": 6, "compansation_level": 22083 },
+												{ "id": 7, "compansation_level": 31250 },
+												{ "id": 8, "compansation_level": 52083 }
+
+											]
+
+                                    },
+                                    {
+                                        "s4":
+											[
+												{ "id": 1, "compansation_level": 1 },
+												{ "id": 2, "compansation_level": 12500 },
+												{ "id": 3, "compansation_level": 13333 },
+												{ "id": 4, "compansation_level": 15000 },
+												{ "id": 5, "compansation_level": 18333 },
+												{ "id": 6, "compansation_level": 24167 },
+												{ "id": 7, "compansation_level": 33333 },
+												{ "id": 8, "compansation_level": 54167 }
+
+											]
+
+                                    },
+                                    {
+                                        "me":
+											[
+												{ "id": 1, "compansation_level": 1 },
+												{ "id": 2, "compansation_level": 4167 },
+												{ "id": 3, "compansation_level": 5000 },
+												{ "id": 4, "compansation_level": 6667 },
+												{ "id": 5, "compansation_level": 10000 },
+												{ "id": 6, "compansation_level": 15833 },
+												{ "id": 7, "compansation_level": 25000 },
+												{ "id": 8, "compansation_level": 45833 }
+
+											]
+
+                                    },
+									{
+									    "me1":
+											[
+												{ "id": 1, "compansation_level": 1 },
+												{ "id": 2, "compansation_level": 6250 },
+												{ "id": 3, "compansation_level": 7083 },
+												{ "id": 4, "compansation_level": 8750 },
+												{ "id": 5, "compansation_level": 12083 },
+												{ "id": 6, "compansation_level": 17917 },
+												{ "id": 7, "compansation_level": 27083 },
+												{ "id": 8, "compansation_level": 47917 }
+
+											]
+
+									},
+                                    {
+                                        "me2":
+											[
+												{ "id": 1, "compansation_level": 1 },
+												{ "id": 2, "compansation_level": 8333 },
+												{ "id": 3, "compansation_level": 9167 },
+												{ "id": 4, "compansation_level": 10833 },
+												{ "id": 5, "compansation_level": 14167 },
+												{ "id": 6, "compansation_level": 20000 },
+												{ "id": 7, "compansation_level": 29167 },
+												{ "id": 8, "compansation_level": 47917 }
+
+											]
+
+                                    },
+                                    {
+                                        "me3":
+											[
+												{ "id": 1, "compansation_level": 1 },
+												{ "id": 2, "compansation_level": 10417 },
+												{ "id": 3, "compansation_level": 11250 },
+												{ "id": 4, "compansation_level": 12917 },
+												{ "id": 5, "compansation_level": 16250 },
+												{ "id": 6, "compansation_level": 22083 },
+												{ "id": 7, "compansation_level": 31250 },
+												{ "id": 8, "compansation_level": 52083 }
+
+											]
+
+                                    },
+                                    {
+                                        "me4":
+											[
+												{ "id": 1, "compansation_level": 1 },
+												{ "id": 2, "compansation_level": 12500 },
+												{ "id": 3, "compansation_level": 13333 },
+												{ "id": 4, "compansation_level": 15000 },
+												{ "id": 5, "compansation_level": 18333 },
+												{ "id": 6, "compansation_level": 24167 },
+												{ "id": 7, "compansation_level": 33333 },
+												{ "id": 8, "compansation_level": 54167 }
+
+											]
+
+                                    }
 								]
 								
 						}
@@ -49,17 +182,29 @@
 				{
 					"monthly":
 						[
-							{ "id": 1, "value": 0,  		"percent": 0 },
-							{ "id": 2, "value": 0,  		"percent": .05},
-							{ "id": 3, "value": 41.67,  	"percent": .1},
-							{ "id": 4, "value": 208.33,  	"percent": .15},
-							{ "id": 5, "value": 708.33,  	"percent": .20},
-							{ "id": 6, "value": 1875,  		"percent": .25},
-							{ "id": 7, "value": 4166.67, 	"percent": .30},
-							{ "id": 8, "value": 10416.67, 	"percent": .32},
-							
+							{ "id": 1, "value": 0 },
+							{ "id": 2, "value": 0 },
+							{ "id": 3, "value": 41.67 },
+							{ "id": 4, "value": 208.33 },
+							{ "id": 5, "value": 708.33 },
+							{ "id": 6, "value": 1875 },	
+							{ "id": 7, "value": 4166.67 },
+							{ "id": 8, "value": 10416.67 }
 						]
-				}
+				},
+                {
+                    "semi-monthly":
+						[
+							{ "id": 1, "value": 0 },
+							{ "id": 2, "value": 0 },
+							{ "id": 3, "value": 20.83 },
+							{ "id": 4, "value": 104.17 },
+							{ "id": 5, "value": 354.17 },
+							{ "id": 6, "value": 937.50 },
+							{ "id": 7, "value": 2083.33 },
+							{ "id": 8, "value": 5208.33 }
+						]
+                }
 			]
 	}
 ]
